9ec97b33a7b7c99d09345c31ae23a0151b6f00d6
Rafael Sabedra <152562421+rafaelssabedra@users.noreply.github.com>
2023-12-18 21:48:02 -0300

    Update test_es.py

--- tests/test_es.py
+++ tests/test_es.py
@@ -1,13 +1,4 @@
-"""
-Módulo Entrada e Saída
-
-Descrição: Este módulo prevê funções de entrada e saída de dados para o Aplicativo de análise de dados via API.
-Autor: Letícia Santos e Ronaldo Debiasi
-Versão: 0.0.1
-Data: 12/12/2023
-
-"""
-# Importando bibliotecas necessárias
+# Importando as bibliotecas
 
 import numpy as np
 import matplotlib as mpl
@@ -20,20 +11,20 @@ def variavel() -> str:
     """ Esta função recebe do usuário a definição que qual variável financeira a ser analisada """
     variavel = input("""
 Qual variável do Balancete de Despesas Orçamentárias dos municípios do RS em 2023 você deseja analisar:
-1 - Valor de Dotação Inicial
-2 - Valor de Créditos Suplementares
+1 - Valor de dotação Inicial
+2 - Valor de Créditos suplementares
 3 - Valor de Créditos Especiais
-4 - Valor de Créditos Extraordinários
+4 - Valor de Créditos Etraordinários
 5 - Valor de Redução de Dotação
-6 - Valor de Transferência
-7 - Valor de Transposição
-8 - Valor de Remanejamento
+6 - Valores de Transferência
+7 - Valores de Transposição
+8 - Valores de Remanejamento
 9 - Valor Empenhado
 10 - Valor Liquidado
 11 - Valor Pago
 12 - Valor Limitado
-13 - Valor de Recomposição
-14 - Valor de Previsão de Execução
+13 - Valores de Recomposição
+14 - Valores de Previsão de Execução
 Digite o código da variável: 
 """)
     return variavel
